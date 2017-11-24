611f0f037c4e579f8a60b4cd32c6fa8dd83e3e50
Dom Christie <christiedom@gmail.com>
2017-11-24 11:19:37 +0000

    Add highlighted code block rules

--- src/gfm.js
+++ src/gfm.js
@@ -1,9 +1,15 @@
+import highlightedCodeBlock from './highlighted-code-block'
 import strikethrough from './strikethrough'
 import tables from './tables'
 import taskListItems from './task-list-items'
 
 function gfm (turndownService) {
-  turndownService.use([strikethrough, tables, taskListItems])
+  turndownService.use([
+    highlightedCodeBlock,
+    strikethrough,
+    tables,
+    taskListItems
+  ])
 }
 
-export { gfm, strikethrough, tables, taskListItems }
+export { gfm, highlightedCodeBlock, strikethrough, tables, taskListItems }
